0fa95800ed5d09e515caf348decc99ed7291d131
danna <dannavila.1809@gmail.com>
2023-09-12 12:15:46 -0500

    Organize packages by the MVP design pattern

---
+++ app/src/main/java/com/dannxvc/todoapp/views/Welcome.java
@@ -0,0 +1,16 @@
+package com.dannxvc.todoapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dannxvc.todoapp.R;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
