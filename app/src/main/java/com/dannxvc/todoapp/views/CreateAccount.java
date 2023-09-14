0e9f2b174a99aa7f0a260a3a808d864558ffe2b9
danna <dannavila.1809@gmail.com>
2023-09-14 16:43:52 -0500

    Complete create account and login activities

---
+++ app/src/main/java/com/dannxvc/todoapp/views/CreateAccount.java
@@ -0,0 +1,16 @@
+package com.dannxvc.todoapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dannxvc.todoapp.R;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
