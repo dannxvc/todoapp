b9063a9447298289e499a5dcb5a8fb7d6e195d22
danna <dannavila.1809@gmail.com>
2023-10-19 17:38:20 -0500

    Update navigation

--- app/src/main/java/com/dannxvc/todoapp/views/CreateAccount.java
+++ app/src/main/java/com/dannxvc/todoapp/views/CreateAccount.java
@@ -2,7 +2,9 @@ package com.dannxvc.todoapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dannxvc.todoapp.R;
 
@@ -12,5 +14,9 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+    public void goToLogin(View view){
+        Intent intent=new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
