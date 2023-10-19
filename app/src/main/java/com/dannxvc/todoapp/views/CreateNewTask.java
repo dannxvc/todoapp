a2c1400b31c2f39a47ea6f65c69cb11d1c560252
danna <dannavila.1809@gmail.com>
2023-10-19 18:16:19 -0500

    Fix buttons positions

--- app/src/main/java/com/dannxvc/todoapp/views/CreateNewTask.java
+++ app/src/main/java/com/dannxvc/todoapp/views/CreateNewTask.java
@@ -2,7 +2,9 @@ package com.dannxvc.todoapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dannxvc.todoapp.R;
 
@@ -12,5 +14,9 @@ public class CreateNewTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_task);
+    }
+    public void goToHome(View view){
+        Intent intent=new Intent(this, HomeTasks.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/dannxvc/todoapp/views/HomeTasks.java
+++ app/src/main/java/com/dannxvc/todoapp/views/HomeTasks.java
@@ -23,4 +23,8 @@ public class HomeTasks extends AppCompatActivity {
         Intent intent=new Intent(this, HomeTasks.class);
         startActivity(intent);
     }
+    public void goToLogin(View view){
+        Intent intent=new Intent(this, Login.class);
+        startActivity(intent);
+    }
 }
